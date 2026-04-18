2afe22c4d5c722bc5635d9093f1b803293a99530
25A31A05GU <charvisri04@gmail.com>
2026-04-18 12:46:54 +0530

    Create program1.cpp

---
+++ program1.cpp
@@ -0,0 +1,10 @@
+//c++ program to print student details
+#include<iostream>
+using namespace std;
+int main()
+{
+  cout<<"Name:Ramesh";
+  cout<<"Roll.No:25A31A05IG";
+  cout<<"Branch:CSE";
+  cout<<"Section:E";
+}
